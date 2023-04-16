6b9f1770af802eeb4e6e5a9d063ad92823fd3a3f
sediqa01 <64917259+sediqa01@users.noreply.github.com>
2023-04-16 11:24:41 +0000

    Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true}/>
+      <NestingComponents />
       
     </div>
   );
--- my-app/src/components/ConditionalRenderingFunctional.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ConditionalRenderingFunctional(props) {
-  return (
-    <h1>{props.connected ? "connected" : "Not connected"}</h1>
-  )
-}
-
-export default ConditionalRenderingFunctional
