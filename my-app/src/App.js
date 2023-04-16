df42a8e1fb436da5a12c032c7b9b907ae15681d5
sediqa01 <64917259+sediqa01@users.noreply.github.com>
2023-04-16 10:48:45 +0000

    class Conditional Rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={false}/>
+      <ConditionalRenderingFunctional connected={true}/>
       
     </div>
   );
--- my-app/src/components/ConditionalRenderingClass.js
+++ my-app/src/components/ConditionalRenderingClass.js
@@ -5,12 +5,28 @@ export class ConditionalRenderingClass extends Component {
       super(props)
     
       this.state = {
+        isLoaded: true,
+        isLoggedIn: true,
       }
     }
   render() {
     return (
-      <h1>Loading</h1>
-    )
+        <div>
+          <h1>{this.state.isLoaded ? "Data loaded!" : "Loading..."}</h1>
+          {this.state.isLoggedIn ? (
+            <div>
+              <p>Welcome to the site! Please complete the steps below:</p>
+              <ol>
+                <li>Confirm your email</li>
+                <li>Complete your profile</li>
+                <li>Subscribe to the newsletter</li>
+              </ol>
+            </div>
+          ) : (
+            <p>Please sign in</p>
+          )}
+        </div>
+      );
   }
 }
 
